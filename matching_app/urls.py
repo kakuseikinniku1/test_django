0a4f5ab7bf2e00ef103ffdb64036f3ba4a4a64a1
kakuseikinniku <kakuseikyuuri@gmail.com>
2026-04-01 04:15:38 +0900

    ユーザーページの作成

--- matching_app/urls.py
+++ matching_app/urls.py
@@ -3,8 +3,9 @@ from django.urls import path
 from django.conf.urls.static import static
 from matching_app.views.index import index
 from matching_app.views.login import login_view, logout_view
+from matching_app.views.recruitment import recruitment_timeline
 from matching_app.views.signup import signup
-from matching_app.views.user_profile import user_home
+from matching_app.views.user_profile import user_home, user_profile_update, user_profile_list, user_profile_detail
 from matching_app.views.verify import send_new_verification_code, verify_email
 
 urlpatterns = (
@@ -16,6 +17,10 @@ urlpatterns = (
         path("login/", login_view, name="login"),
         path("logout/", logout_view, name="logout"),
         path("home/", user_home, name="user_home"),
+        path("profiles/me/update/", user_profile_update, name="user_profile_update"),
+        path("profiles/list/", user_profile_list, name="user_profile_list"),
+        path("profiles/<int:pk>/", user_profile_detail, name="user_profile_detail"),
+        path("recruitments/", recruitment_timeline, name="recruitment_timeline"),
     ]
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
